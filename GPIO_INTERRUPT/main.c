7eb6c7c1035bf46ba7c109cc63ad24a460b3e4fe
Ksrind1208 <minhducm1208@gmail.com>
2024-09-23 23:02:54 +0700

    Change some texts

--- GPIO_INTERRUPT/main.c
+++ GPIO_INTERRUPT/main.c
@@ -6,16 +6,20 @@ void delay_ms(){
 	for(volatile uint32_t i=0;i<2500000;i++);// mapping variable for avoid optimization
 }
 int main(void){
-	
-	/*RCC_APB2ENR|=1<<4;
-	GPIOC->CRH&=0xFF0FFFFF; //reset CRH port B
-	GPIOC->CRH|=0x00300000; //output max 50Mhz*/
-	
+	RCC->APB2ENR|=1<<0;// enable clock afio
 	init_GP(PC,13,OUT10,O_GP_PP);
 	init_GP(PB,5,IN,I_PP);
 	GPIOB->ODR|=1<<5;
+	/*RCC->APB2ENR|=(1<<3) | (1<<4) | (1<<0);
+	GPIOB->CRL&=0xFF0FFFFF;
+	GPIOB->CRL|=0x00800000;
+	GPIOB->ODR|=1<<5;
+
+	GPIOC->CRH&=0xFF0FFFFF;
+	GPIOC->CRH|=0x00300000;*/
 	
 	__disable_irq();
+	AFIO->EXTICR[1] &= 0xFF0F;// clear exti line 5 
 	AFIO->EXTICR[1]=0x0010;
 	EXTI->IMR|=1<<5;
 	EXTI->FTSR|=1<<5;
@@ -29,9 +33,11 @@ int main(void){
 			W_GP(PC,13,1);
 		}*/
 		if(signal){
-			W_GP(PC,13,0);
+			//W_GP(PC,13,0);
+			GPIOC->BSRR=1<<29;
 		}else{
-			W_GP(PC,13,1);
+			//W_GP(PC,13,1);
+			GPIOC->BSRR=1<<13;
 		}
 	}
 	return 0;
